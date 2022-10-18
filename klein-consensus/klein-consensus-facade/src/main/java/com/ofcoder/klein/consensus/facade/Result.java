3feb76ebf57fd7b8814aa0bb05f271eba678dc9e
shihuili1218 <gege87417376@qq.com>
2022-10-18 20:08:13 +0800

    init

---
+++ klein-consensus/klein-consensus-facade/src/main/java/com/ofcoder/klein/consensus/facade/Result.java
@@ -0,0 +1,10 @@
+package com.ofcoder.klein.consensus.facade;
+
+/**
+ * @author: 释慧利
+ */
+public enum Result {
+    SUCCESS,
+    UNKNOWN
+
+}
---
+++ klein-core/src/main/java/com/ofcoder/klein/core/config/KleinProp.java
@@ -0,0 +1,16 @@
+package com.ofcoder.klein.core.config;
+
+/**
+ * klein:
+ *   storage: jvm
+ *   max-size:
+ *
+ *
+ *
+ * @author: 释慧利
+ */
+public class KleinProp {
+
+
+
+}
