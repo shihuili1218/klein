66cc419d493879a519da64aedbd518063efeecb2
tynan.liu <tynan.liu@xinjifamily.com>
2022-10-20 17:41:24 +0800

    grpc client

---
+++ klein-common/src/main/java/com/ofcoder/klein/common/util/ThreadExecutor.java
@@ -0,0 +1,24 @@
+package com.ofcoder.klein.common.util;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: 释慧利
+ */
+public class ThreadExecutor {
+    // fixme to user custom
+    private final static ExecutorService EXECUTOR = new ThreadPoolExecutor(10, 30,
+            300L, TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<>(10000),
+            com.xinji.top.common.concurrent.ThreadFactory.create("audit-predict", true));
+
+
+    public static void submit(Runnable task){
+        EXECUTOR.submit(task);
+    }
+
+
+}
---
+++ klein-rpc/klein-rpc-facade/src/main/java/com/ofcoder/klein/rpc/facade/exception/ConnectionException.java
@@ -0,0 +1,14 @@
+package com.ofcoder.klein.rpc.facade.exception;
+
+/**
+ * @author: 释慧利
+ */
+public class ConnectionException extends RpcException{
+    public ConnectionException(String message) {
+        super(message);
+    }
+
+    public ConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
