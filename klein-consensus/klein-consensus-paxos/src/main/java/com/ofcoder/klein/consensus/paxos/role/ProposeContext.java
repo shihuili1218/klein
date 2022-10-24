81d4d15f5dd0575239c5b5542bddf707b61a04af
shihuili1218 <gege87417376@qq.com>
2022-10-24 18:07:03 +0800

    refactor prepare

---
+++ klein-consensus/klein-consensus-paxos/src/main/java/com/ofcoder/klein/consensus/paxos/role/ProposeContext.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ofcoder.klein.consensus.paxos.role;
+
+import com.ofcoder.klein.consensus.facade.Quorum;
+import com.ofcoder.klein.consensus.paxos.PaxosQuorum;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author: 释慧利
+ */
+public class ProposeContext {
+    private long instanceId;
+    private List<ByteBuffer> datas;
+    private int times = 0;
+    private Quorum prepareQuorum;
+    private AtomicBoolean prepareNexted;
+    private Quorum acceptQuorum;
+    private AtomicBoolean acceptNexted;
+
+    public ProposeContext(long instanceId, List<ByteBuffer> datas) {
+        this.instanceId = instanceId;
+        this.datas = datas;
+        reset();
+    }
+
+    public int getTimesAndIncrement() {
+        return times++;
+    }
+
+    public void reset() {
+        this.prepareQuorum = PaxosQuorum.createInstance();
+        this.prepareNexted = new AtomicBoolean(false);
+        this.acceptQuorum = PaxosQuorum.createInstance();
+        this.acceptNexted = new AtomicBoolean(false);
+    }
+
+
+    public long getInstanceId() {
+        return instanceId;
+    }
+
+    public List<ByteBuffer> getDatas() {
+        return datas;
+    }
+
+    public void setDatas(List<ByteBuffer> datas) {
+        this.datas = datas;
+    }
+
+    public int getTimes() {
+        return times;
+    }
+
+    public Quorum getPrepareQuorum() {
+        return prepareQuorum;
+    }
+
+    public AtomicBoolean getPrepareNexted() {
+        return prepareNexted;
+    }
+
+    public Quorum getAcceptQuorum() {
+        return acceptQuorum;
+    }
+
+    public AtomicBoolean getAcceptNexted() {
+        return acceptNexted;
+    }
+}
