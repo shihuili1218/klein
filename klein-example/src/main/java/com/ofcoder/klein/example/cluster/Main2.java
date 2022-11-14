9c5bf84f1aa871073c900be517f1d04a1ba7a5a2
tynan.liu <tynan.liu@xinjifamily.com>
2022-11-14 14:20:21 +0800

    add/remove member

--- klein-example/src/main/java/com/ofcoder/klein/example/cluster/Main2.java
+++ klein-example/src/main/java/com/ofcoder/klein/example/cluster/Main2.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ofcoder.klein.example;
+package com.ofcoder.klein.example.cluster;
 
 import java.io.IOException;
 
