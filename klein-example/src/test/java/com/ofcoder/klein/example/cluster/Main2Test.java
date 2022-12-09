ded00fa74407e138e3c96395959581d49c0691f6
yzker <gege87417376@qq.com>
2022-12-09 21:39:04 +0800

    refactor learn

---
+++ klein-example/src/test/java/com/ofcoder/klein/example/cluster/Main2Test.java
@@ -0,0 +1,14 @@
+package com.ofcoder.klein.example.cluster;
+
+import junit.framework.TestCase;
+
+/**
+ * @author far.liu
+ */
+public class Main2Test extends TestCase {
+
+    public void test() {
+
+
+    }
+}
