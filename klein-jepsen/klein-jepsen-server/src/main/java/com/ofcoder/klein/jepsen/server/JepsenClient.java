6bede146ed5e72da3d8d78cbe71408510a288bd2
shihuili <gege87417376@qq.com>
2023-01-18 18:00:56 +0800

    jepsen client

--- klein-jepsen/klein-jepsen-server/src/main/java/com/ofcoder/klein/jepsen/server/JepsenClient.java
+++ klein-jepsen/klein-jepsen-server/src/main/java/com/ofcoder/klein/jepsen/server/JepsenClient.java
@@ -18,8 +18,16 @@ package com.ofcoder.klein.jepsen.server;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ofcoder.klein.jepsen.server.rpc.PutReq;
+import com.ofcoder.klein.rpc.facade.Endpoint;
+import com.ofcoder.klein.rpc.facade.util.RpcUtil;
+import com.ofcoder.klein.rpc.grpc.GrpcClient;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * jepsen‘s client.
@@ -31,8 +39,45 @@ public class JepsenClient {
     static final Logger LOG = LoggerFactory.getLogger(KleinServer.class);
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
             .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+    private final List<Endpoint> endpoints;
+    private final GrpcClient client;
 
     public JepsenClient(final String clusterInfo) {
+        LOG.info("clusterInfo: {}", clusterInfo);
+        endpoints = parseMember(clusterInfo);
+        client = new GrpcClient();
+        for (Endpoint endpoint : endpoints) {
+            client.createConnection(endpoint);
+        }
+    }
 
+    /**
+     * put.
+     *
+     * @param key   key
+     * @param value value
+     * @return result
+     */
+    public boolean put(final String key, final String value) {
+        PutReq req = new PutReq();
+        req.setData(value);
+        req.setKey(key);
+        boolean o = client.sendRequestSync(endpoints.get(0), req, 1000);
+        LOG.info("put result: {}", o);
+        return o;
+    }
+
+    private List<Endpoint> parseMember(final String members) {
+        List<Endpoint> endpoints = new ArrayList<>();
+        if (StringUtils.isEmpty(members)) {
+            return endpoints;
+        }
+        for (String item : StringUtils.split(members, ";")) {
+            if (StringUtils.isEmpty(item)) {
+                continue;
+            }
+            endpoints.add(RpcUtil.parseEndpoint(item));
+        }
+        return endpoints;
     }
 }
--- klein-jepsen/klein-jepsen-test/src/jepsen/kelin.clj
+++ klein-jepsen/klein-jepsen-test/src/jepsen/kelin.clj
@@ -18,9 +18,6 @@
             [jepsen.control.util :as cu]
             [jepsen.os :as os]))
 
-(defn r   [_ _] {:type :invoke, :f :read, :value nil})
-(defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
-
 (def fn-opts [[nil "--testfn TEST" "Test function name."]])
 
 (defn- parse-long [s] (Long/parseLong s))
@@ -30,7 +27,7 @@
   "Additional command line options."
   [["-s"
     "--slots SLOTS"
-    "Number of atomic server ranges."
+    "Number of klein server ranges."
     :default  1
     :parse-fn parse-long
     :validate [pos? "Must be a positive number"]]
@@ -53,28 +50,88 @@
     :parse-fn parse-long
     :validate [pos? "Must be a positive integer."]]])
 
-(defn- create-client0 [test] (doto (JepsenClient. "1:172.22.0.79:1218;2:172.22.0.80:1218;3:172.22.0.90:1218;4:172.22.0.91:1218;5:172.22.0.96:1218")))
+;;DB
+(defn db
+  "klein DB for a particular version."
+  [version]
+  (reify
+   db/DB
+   (setup! [_ test node]
+           (info node "installing klein" version))
+
+   (teardown! [_ test node]
+              (info node "tearing down klein"))))
+
+;client
+(defn r   [_ _] {:type :invoke, :f :read, :value nil})
+(defn w   [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
+
+(defn- create-client0 [test]
+  (doto
+   (JepsenClient. "1:172.22.0.79:1218;2:172.22.0.80:1218;3:172.22.0.90:1218;4:172.22.0.91:1218;5:172.22.0.96:1218")))
+
 (def create-client (memoize create-client0))
 
+(defn- write
+  "write a key/value to klein server"
+  [client key value]
+  (when-not
+    (-> client
+        :conn
+        (.put key value))
+    (throw (ex-info "Fail to set" {:key key :value value}))))
+
 (defrecord Client [conn]
-  client/Client
-  (open! [this test node]
-    (-> this
-        (assoc :node node)
-        (assoc  :conn (create-client test))))
+  (reify client/Client
+    (open! [this test node]
+           (-> this
+               (assoc :node node)
+               (assoc :conn (create-client test))))
 
-  (setup! [this test])
+    (setup! [this test])
 
-  (invoke! [_ test op])
+    (invoke! [this test op]
+             (let [[kk v] (:value op)
+                   k      (str kk)
+                   crash  (if (= :read (:f op)) :fail :info)]
+               (try
+                 (case (:f op)
+                   :write (do
+                            (write this k v)
+                            (assoc op :type :ok)))
+                 (catch Exception e
+                   (let [^String msg (.getMessage e)]
+                     (cond
+                       (and msg (.contains msg "TIMEOUT")) (assoc op :type crash, :error :timeout)
+                       :else
+                       (assoc op :type crash :error (.getMessage e))))))))
 
-  (teardown! [this test])
+    (teardown! [this test])
 
-  (close! [_ test]))
+    (close! [_ test])))
+
+(defn klein-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh,
+  :concurrency, ...), constructs a test map."
+  [opts]
+  (merge tests/noop-test
+         {:pure-generators true
+          :name            "klein"
+          :db              (db "0.0.1")
+          :client          (Client nil)
+          :generator       (->> (gen/mix [r w])
+                                (gen/stagger 1)
+                                (gen/nemesis nil)
+                                (gen/time-limit 15))}
+         opts))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
   browsing results."
   [& args]
-  (ns jepsen.kelin)
-  (println "Hello, klein")
-  )
+  (info "hello klein")
+  (cli/run!
+   (merge (cli/single-test-cmd {:test-fn klein-test})
+          (cli/serve-cmd))
+   args))
+
