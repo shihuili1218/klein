1f71df0918fc41ae788998c698a53c516b6ed49c
shihuili1218 <gege87417376@qq.com>
2023-02-22 21:21:08 +0800

    update prop

--- klein-jepsen/klein-jepsen-server/deploy/start.sh
+++ klein-jepsen/klein-jepsen-server/deploy/start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -x
 
-java -Xmx512m -Xms512m  \
+nohup java -Xmx512m -Xms512m  \
   -Dklein.id=${KLEIN_ID}  \
   -Dklein.port=${KLEIN_PORT}  \
   -Dklein.ip=${KLEIN_IP}  \
   -Dklein.members=${KLEIN_MEMBERS} \
-  -jar /klein/klein-server.jar
+  -jar /klein/klein-server.jar \
+  $@ >> klein.log 2>&1 &
+
+echo "Done!"
