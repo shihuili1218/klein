169278a885ca2ebf98f14dddba9eaff01faf0315
shihuili <gege87417376@qq.com>
2023-02-23 11:12:59 +0800

    fix docker env

--- klein-jepsen/klein-jepsen-server/deploy/start.sh
+++ klein-jepsen/klein-jepsen-server/deploy/start.sh
@@ -2,10 +2,6 @@
 set -x
 
 nohup java -Xmx512m -Xms512m  \
-  -Dklein.id=${KLEIN_ID}  \
-  -Dklein.port=${KLEIN_PORT}  \
-  -Dklein.ip=${KLEIN_IP}  \
-  -Dklein.members=${KLEIN_MEMBERS} \
   -jar /klein/klein-server.jar \
   $@ >> klein.log 2>&1 &
 
